95ddd07c923f73b06c5e927585056da60b62e15d
Naveed Zaheer <nzaheer@microsoft.com>
2020-08-10 22:07:50 -0500

    addded new functions

--- RatingAPIFunc/RatingAPIFuncApp.cs
+++ RatingAPIFunc/RatingAPIFuncApp.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Net.Http;
+using Microsoft.Azure.WebJobs.Host;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RatingAPIFunc
 {
@@ -15,7 +18,7 @@ namespace RatingAPIFunc
     {
         // Comments-1
         [FunctionName("CreateRating")]
-        public static async Task<IActionResult> Run(
+        public static async Task<IActionResult> CreateRating(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             [CosmosDB(
             databaseName: "ratings",
@@ -31,12 +34,53 @@ namespace RatingAPIFunc
             if (isProductValid && isUserValid)
             {
                 rating.id = Guid.NewGuid().ToString();
-                rating.timestamp = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                rating.timestamp = DateTime.Now.ToString("u");
                 await ratingsOut.AddAsync(rating);
                 return new OkObjectResult(JsonConvert.SerializeObject(rating));
             }
 
             return new BadRequestResult();
+        }
+
+        [FunctionName("GetRating")]
+        public static async Task<IActionResult> GetRating(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            [CosmosDB(
+            databaseName: "ratings",
+            collectionName: "ratings",
+            ConnectionStringSetting = "RatingsDBConnection",
+            SqlQuery = "select * from ratings r")]IEnumerable<Rating> ratings,
+            ILogger log)
+        {
+            string ratingId = req.Query["ratingId"];
+
+            if (ratings.Count() > 0 && ratings.FirstOrDefault(r => r.id == ratingId) != null)
+            {
+                return new OkObjectResult(ratings.First(r => r.id == ratingId));
+            }
+
+            return new NotFoundObjectResult("No ratings found for ratingId:" + ratingId);
+        }
+
+        [FunctionName("GetRatings")]
+        public static async Task<IActionResult> GetRatings(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            [CosmosDB(
+            databaseName: "ratings",
+            collectionName: "ratings",
+            ConnectionStringSetting = "RatingsDBConnection",
+            SqlQuery = "select * from ratings r")]IEnumerable<Rating> ratings,
+            ILogger log)
+        {
+            string userId = req.Query["userId"];
+            bool isUserValid = await IsUserIdValidAsync(userId);
+
+            if (ratings.Count() > 0 && (ratings.FirstOrDefault(r => r.userId == userId) != null) && isUserValid)
+            {
+                return new OkObjectResult(ratings.Where(r => r.userId == userId).ToList());
+            }
+
+            return new NotFoundObjectResult("No ratings found for userId:" + userId);
         }
 
         private static async Task<bool> IsUserIdValidAsync(string userId)
